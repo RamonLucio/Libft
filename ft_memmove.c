1f2d77050343fb66782083de6312deed36c4376e
Ramon <ramonluciolinhares@gmail.com>
2021-07-28 15:50:38 -0300

    ft_memmove.c

---
+++ ft_memmove.c
@@ -0,0 +1,19 @@
+void	*memmove(void *dst, const void *src, size_t len)
+{
+	char		*destination;
+	const char	*source;
+
+	destination = (char *) dst;
+	source = (char *) src;
+	if (destination < source)
+		while (len--)
+			*destination++ = *source++;
+	else
+	{
+		destination += (len - 1);
+		source += (len - 1);
+		while (len--)
+			*destination-- = *source--;
+	}
+	return (dst);
+}
