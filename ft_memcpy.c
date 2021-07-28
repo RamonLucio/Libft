8584c35debe8663162f17ec06787ba638c99271e
Ramon <ramonluciolinhares@gmail.com>
2021-07-28 13:17:10 -0300

    ft_memcpy.c

---
+++ ft_memcpy.c
@@ -0,0 +1,11 @@
+void	*memcpy(void *dst, const void *src, size_t n)
+{
+	char		*destination;
+	const char	*source;
+
+	destination = (char *) dst;
+	source = (char *) src;
+	while (n--)
+		*destination++ = *source++;
+	return (dst);
+}
