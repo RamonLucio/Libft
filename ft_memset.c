cdc359f67bdfbdc6dcc1d4417567ec9f5b3c0cb8
Ramon <ramonluciolinhares@gmail.com>
2021-07-28 10:15:21 -0300

    Removes variable; casting argument on assignment

--- ft_memset.c
+++ ft_memset.c
@@ -1,11 +1,9 @@
 void	*memset(void *b, int c, size_t len)
 {
 	unsigned char	*str;
-	unsigned char	value;
 
 	str = (unsigned char *) b;
-	value = (unsigned char) c;
 	while (len-- > 0)
-		*str++ = value;
+		*str++ = (unsigned char) c;
 	return (b);
 }
