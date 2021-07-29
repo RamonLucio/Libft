8fda338f0e6f6a51efee0353008d21a9bcecd261
Ramon <ramonluciolinhares@gmail.com>
2021-07-29 18:08:34 -0300

    ft_strlcat

---
+++ ft_strlcat.c
@@ -0,0 +1,22 @@
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	char		*destination;
+	const char	*source;
+	size_t		size;
+	size_t		dst_len;
+
+	destination = dst;
+	source = src;
+	size = dstsize;
+	while (size-- != 0 && *destination != '\0')
+		destination++;
+	dst_len = destination - dst;
+	size = dstsize - dst_len;
+	if (n == 0)
+		return (dst_len + ft_strlen(s));
+	while (*source++ != '\0')
+		if (size-- != 1)
+			*destination++ = *source;
+	*destination = '\0';
+	return (dst_len + (source - src));
+}
