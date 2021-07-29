f519e1915c6e0cd81b6916c1107259df911417d6
Ramon <ramonluciolinhares@gmail.com>
2021-07-29 09:34:44 -0300

    Fix function name and write in accordance with the Norm

---
+++ ft_strlcpy.c
@@ -0,0 +1,28 @@
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	char		*destination;
+	const char	*source;
+	size_t		destination_size;
+
+	destination = dst;
+	source = src;
+	destination_size = dstsize;
+	if (destination_size != 0)
+	{
+		while (--destination_size)
+		{
+			if (*source++ == '\0')
+				break ;
+			else
+				(*destination++ = *source++);
+		}
+	}
+	if (destination_size == 0)
+	{
+		if (dstsize != 0)
+			*destination = '\0';
+		while (*source++)
+			;
+	}
+	return (source - src - 1);
+}
