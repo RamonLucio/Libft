a5851383b7d6c731a34dcee288e17197ba09fb24
Ramon <ramonluciolinhares@gmail.com>
2021-07-30 10:41:49 -0300

    ft_strrchr.c

---
+++ ft_strrchr.c
@@ -0,0 +1,14 @@
+char	*ft_strrchr(const char *s, int c)
+{
+	const char	*ptr;
+	char		character;
+	char		*return_value;
+
+	ptr = s;
+	character = (char) c;
+	return_value = NULL;
+	while (*ptr++)
+		if (*ptr == character)
+			return_value = (char *) ptr;
+	return (return_value);
+}
