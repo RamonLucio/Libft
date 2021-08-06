0d5309e024cd9e7c59013a3bb54a6487ed303328
Ramon <ramonluciolinhares@gmail.com>
2021-08-06 13:57:38 -0300

    Fix unary operators logic

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -2,13 +2,13 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	const unsigned char	*string1;
-	const unsigned char	*string2;
+	unsigned char	*string1;
+	unsigned char	*string2;
 
-	string1 = (const unsigned char *) s1;
-	string2 = (const unsigned char *) s2;
-	while (n-- != 0)
+	string1 = (unsigned char *) s1;
+	string2 = (unsigned char *) s2;
+	while (n--)
 		if (*string1++ != *string2++)
-			return (*string1 - *string2);
+			return (*--string1 - *--string2);
 	return (0);
 }
