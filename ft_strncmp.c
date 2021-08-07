73e333b851280bc771edf628086376f8a431c6cb
Ramon <ramonluciolinhares@gmail.com>
2021-08-07 12:01:45 -0300

    Fix function

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -7,8 +7,12 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 
 	string1 = (unsigned char *) s1;
 	string2 = (unsigned char *) s2;
-	while (n-- && ((*string1++ != '\0') && (*string2++ != '\0')))
-		if (*string1 != *string2)
-			return (string1 - string2);
+	while (n-- > 0)
+	{
+		if (*string1 != *string2 || *string1 == '\0' || *string2 == '\0')
+			return (*string1 - *string2);
+		string1++;
+		string2++;
+	}
 	return (0);
 }
