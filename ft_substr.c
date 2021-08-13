6906ecc21c09f3385c8119dcecbd23a69cf56a3a
Ramon <ramonluciolinhares@gmail.com>
2021-08-13 09:21:07 -0300

    Fix ft_substr's logic

--- ft_substr.c
+++ ft_substr.c
@@ -3,24 +3,21 @@
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	size_t	index;
-	size_t	substr_len;
+	size_t	str_len;
 	char	*substr;
 
-	index = 0;
-	if (s == NULL)
-		return (NULL);
-	substr_len = ft_strlen(s);
-	if (substr_len < len)
-		substr = (char *) malloc((substr_len + 1) * sizeof(char));
+	str_len = ft_strlen(s);
+	if (str_len > start)
+	{
+		index = 0;
+		while ((index < len) && ((start + index) < str_len))
+			index++;
+		substr = (char *) malloc((index + 1) * sizeof(char));
+	}
 	else
-		substr = (char *) malloc((len + 1) * sizeof(char));
+		return (ft_strdup(""));
 	if (substr == NULL)
 		return (NULL);
-	while ((index < len) && ((start + 1) < substr_len))
-	{
-		substr[index] = s[start + index];
-		index++;
-	}
-	substr[index] = '\0';
+	ft_strlcpy(substr, &s[start], index + 1);
 	return (substr);
 }
