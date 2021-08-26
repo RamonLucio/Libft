40e4c59f6a4ed078dfc861aba119bf8304381705
Ramon <ramonluciolinhares@gmail.com>
2021-08-26 15:23:22 -0300

    ft_memcpy: handle NULL parameter

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: rlucio-l <rlucio-l@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/24 16:28:19 by rlucio-l          #+#    #+#             */
-/*   Updated: 2021/08/24 16:28:21 by rlucio-l         ###   ########.fr       */
+/*   Updated: 2021/08/26 15:15:57 by rlucio-l         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	char		*destination;
 	const char	*source;
 
+	if (!dst || !src)
+		return (NULL);
 	destination = (char *) dst;
 	source = (char *) src;
 	while (n--)
